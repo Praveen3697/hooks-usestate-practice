35b7949546dfba425e298ac71b7cb9a05645006d
Praveen Nair <nairpraveen1997@gmail.com>
2023-06-14 12:32:59 +0530

    Practiced How to use useState Hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
+
+  function currTime() {
+    let newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
+  }
+
+  setInterval(currTime, 1000);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>{time}</h1>
+      <button onClick={currTime}>Get Time</button>
     </div>
   );
 }
